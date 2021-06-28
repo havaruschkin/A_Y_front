760d814a21e16bc8e8b9f7c5b9dd92bb72f6f3cc
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2021-06-28 21:10:28 +0300

    Добавил главную страницу

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import NavbarHead from "./components/Navbar/navbar";
+import MainPage from "./components/Layout/MainPage";
 
 function App() {
   return (
       <div className="wrapper">
           <NavbarHead/>
+          <MainPage/>
       </div>
   )
 }
---
+++ src/components/Layout/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main.scss';
+
+function MainPage () {
+    return (
+        <div className='bg-image'>
+
+        </div>
+
+    )
+}
+
+export default MainPage;
